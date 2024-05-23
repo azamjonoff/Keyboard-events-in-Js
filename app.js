1fbc046053d00d50e472d77f55ec89a2c19b00c2
azamjonoff <abdullohazamjonov57gmail.com>
2024-05-23 10:02:04 +0500

    3-lesson ready

---
+++ app.js
@@ -0,0 +1,38 @@
+// const colors = ["#543310", "#A1DD70", "#240750", "#F6FAB9", "#151515"];
+
+// const items = document.getElementsByTagName("li");
+
+// let numbers = [0, 1, 2, 3, 4];
+
+// for (let i = 0; i < colors.length; i++) {
+//   const randomNumber = numbers[Math.floor(Math.random() * colors.length)];
+//   items[i].style.color = colors[randomNumber];
+//   numbers = [...numbers.filter((num) => num != randomNumber)];
+// }
+
+// UYGA VAZIFA
+
+// 1) KEYBOARD EVENTS
+
+// const insert = document.getElementById("insert");
+
+// window.addEventListener("keydown", (e) => {
+//   insert.innerHTML = `
+//   <div class="key">
+//     ${e.key}
+//     <small>event.key</small>
+//   </div>
+//   <div class="key">
+//     ${e.which}
+//     <small>event.keyCode</small>
+//   </div>
+//   <div class="key">
+//     ${e.code}
+//     <small>event.Code</small>
+//   </div>
+//   `;
+// });
+
+// BIRINCHI OZIM BAJARDIM CHALA JOYLARINI VIDEODAN TOGIRLADIM!
+
+// 2) 
